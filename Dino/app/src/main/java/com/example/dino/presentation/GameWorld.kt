93d682ccc69ac2939b1f00b12b3e2faa32092e3f
Ataul Munim <ataulm@google.com>
2022-11-24 14:57:49 +0000

    Add rsb support

--- Dino/app/src/main/java/com/example/dino/presentation/GameWorld.kt
+++ Dino/app/src/main/java/com/example/dino/presentation/GameWorld.kt
@@ -1,23 +1,39 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.example.dino.presentation
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 
 @ExperimentalLifecycleComposeApi
 @Composable
 fun GameWorld(
     viewModel: GameWorldViewModel = viewModel()
 ) {
+    val focusRequester = remember { FocusRequester() }
+    val coroutineScope = rememberCoroutineScope()
     val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
     Canvas(
         modifier = Modifier
             .fillMaxSize()
@@ -29,7 +45,15 @@ fun GameWorld(
                 detectTapGestures {
                     viewModel.onReceiveJumpInput()
                 }
-            },
+            }
+            .onRotaryScrollEvent {
+                coroutineScope.launch {
+                    viewModel.onReceiveJumpInput()
+                }
+                true
+            }
+            .focusRequester(focusRequester)
+            .focusable(true),
         onDraw = {
             val uiStateValue = uiState.value
             if (uiStateValue != null) {
